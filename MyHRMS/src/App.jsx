25c465d8525330b70724e93de4c7b51140ef0263
Rushikamgr9 <rushikamagar9@gmail.com>
2025-07-14 09:45:22 +0545

    Protected Route Completed

--- MyHRMS/src/App.jsx
+++ MyHRMS/src/App.jsx
@@ -4,13 +4,18 @@ import LoginForm from './pages/login';
 import Home from './pages/home';
 import EmployeeForm from './pages/employee';
 import Dashboard from './pages/dashboard';
+import ProtectedRoute from './components/utils/protectedRoute';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LoginForm/>} />
-        <Route path="home" element={<Home />}>
+        <Route path="home" element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }>
           <Route path="employee" element={<EmployeeForm />} />
           <Route path="dashboard" element={<Dashboard />} />
         </Route>
